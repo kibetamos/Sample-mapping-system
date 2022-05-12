1559e794326d88598f1cd5f4d2f342c50c205012
kibet kilel <amos.rotich@strathmore.edu>
2022-05-12 19:12:01 +0300

    Update the documents

---
+++ temp/search.php
@@ -0,0 +1,8 @@
+<form action="" method="GET" name="">
+	<table>
+		<tr>
+			<td><input type="text" name="k" value="<?php echo isset($_GET['k']) ? $_GET['k'] : ''; ?>" placeholder="Enter your search keywords" /></td>
+			<td><input type="submit" name="" value="Search" /></td>
+		</tr>
+	</table>
+</form>
